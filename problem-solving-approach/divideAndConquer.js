1b1cf231edd9e4e45b56ffc2f0ef0b3d0b500b56
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-10-08 10:42:12 +0100

    Understand the concept of Divide and Conquer

---
+++ problem-solving-approach/divideAndConquer.js
@@ -0,0 +1,6 @@
+/**
+ * Divide can tremendously decrease time complexity
+ *
+ * It involves dividing a data set into small chunks and then repeating
+ * a process with a subset of data until you find the desired result
+ */
