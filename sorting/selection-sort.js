778cf6c97855509d7fe20308730949dd22434ec2
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-10-11 10:11:59 +0100

    Leared selectio sort

---
+++ sorting/selection-sort.js
@@ -0,0 +1,22 @@
+/**
+ * SELECTION SORT is like Bubble sort but it places large values at the beginning
+ * of the array rather than at the end of the array like Bubble sort does
+ */
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[lowest];
+    arr[lowest] = temp;
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([34, 22, 39, 64, 11, 12, 14, 18, 22]));
