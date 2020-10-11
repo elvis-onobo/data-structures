77d4e61ee01ca929171b6487bc322f5864fa10c1
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-10-11 09:45:48 +0100

    Fix swapping of items in the array

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -4,16 +4,22 @@
  */
 
 function bubbleSort(arr) {
+  let noSwaps; // var to prevent loop from running if sort finishes early
   // Get the total lenght and decrement it after each item is finished being sorted
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       // now compare and switch position
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
-        arr[j] = temp;
+        arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
+
+console.log(bubbleSort([5, 1, 2, 3, 8, 9]));
