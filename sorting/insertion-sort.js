21a7d2fee47f91f9e335b7e8447469c98a9fbc84
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-10-12 22:50:06 +0100

    Learned insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,16 @@
+/**
+ * INSERTION SORT builds up the sort by gradually creating a larger left half which
+ * is always sorted
+ */
+function insertionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const currentVal = arr[i];
+        for (var j = i -1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j +1] = arr[j]
+        }
+        arr[j + 1] = currentVal
+    }
+    return arr
+}
+
+console.log(insertionSort([4, 8, 9, 21, 64, 112, 12, 5]));
